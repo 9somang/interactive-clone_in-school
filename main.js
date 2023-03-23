5940bdf9f80610ce4a0986eab02956f8e8bd5a7c
somang Ku <ksmang09@naver.com>
2023-03-24 08:28:35 +0900

    chapter 3
    end

--- main.js
+++ main.js
@@ -1,5 +1,22 @@
 (() => {
 
+    const actions ={
+        birdFlies(key){
+            if(key){
+                document.querySelector('[data-index="2"] .bird').style.transform= `translateX(${window.innerWidth}px)` ;
+            }else{
+                document.querySelector('[data-index="2"] .bird').style.transform= `translateX(-100%)` ;
+            }
+        },
+        birdFlies2(key){
+            if(key){
+                document.querySelector('[data-index="5"] .bird').style.transform= `translate(${window.innerWidth}px, ${-window.innerHeight * 0.7}px)` ;
+            }else{
+                document.querySelector('[data-index="5"] .bird').style.transform= `translateX(-100%)` ;
+            }
+        }
+    }
+
     const stepElems = document.querySelectorAll('.step');
     const graphicElems = document.querySelectorAll('.graphic-item');
     // 현재 활성화된(visible 클래스가 붙은) .graphic-item을 지정
@@ -19,19 +36,24 @@
         graphicElems[i].dataset.index = i;
     }
 
-    function activate() {
+    function activate(action) {
         currentItem.classList.add('visible');
+        if(action){
+            actions[action](true);
+        }
     }
 
-    function inactivate() {
+    function inactivate(action) {
         currentItem.classList.remove('visible');
+        if(action){
+            actions[action](false);
+        }
     }
 
     
     window.addEventListener('scroll', ()=> {
         let step;
         let boundingRect;
-        let temp=0;
 
         // for (let i = 0; i < stepElems.length; i++){
             for(let i = ioIndex -1; i< ioIndex + 2; i++){
@@ -39,20 +61,25 @@
             
             if(!step) continue;
             boundingRect = step.getBoundingClientRect();
-            
-            temp++;
-            console.log(temp);
+        
             
             if(boundingRect.top > window.innerHeight * 0.1 &&
             boundingRect.top < window.innerHeight * 0.8 ) {
                 
                 inactivate();
                 currentItem = graphicElems[step.dataset.index];
-                activate();
+                activate(currentItem.dataset.action);
 
             }
         }
     })
+
+    window.addEventListener('load', ()=> {
+        setTimeout(() => scrollTo(0, 0), 100);
+    });
+
+
+    activate();
 
 })();
 
